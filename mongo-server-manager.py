facfdd4b9b1746f1cc52ba88beece701b6a4639c
root <root@debian-7-x64>
2015-01-05 10:58:10 +0800

    finish mongo boot

--- mongo-server-manager.py
+++ mongo-server-manager.py
@@ -2,8 +2,11 @@ __author__ = 'hzyuxin'
 
 import yaml
 import os
+import sh
+
 
 class MongoSeverManager(object):
+
     def __init__(self, fp):
         with open(fp) as f:
             self.yaml = yaml.load(f)
@@ -25,10 +28,11 @@ class MongoSeverManager(object):
                 self.cmd.append(self.mongos)
             else:
                 Exception('unsupported cmd type')
-        self.cmd = sorted(self.cmd, key = lambda cmd:cmd.priority, reverse=True)
+        self.cmd = sorted(self.cmd, key=lambda cmd: cmd.priority, reverse=True)
 
 
 class MongoCmd(object):
+
     def __init__(self, d, mgr):
         self.data = d
         self.mgr = mgr
@@ -37,38 +41,61 @@ class MongoCmd(object):
     def cmd(self):
         return 'TODO'
 
+    def precmd(self):
+        if hasattr(self, 'path') and not os.path.isdir(self.path):
+            sh.mkdir('-p', self.path)
+
     def bind(self):
         for k, v in self.data.items():
             if v:
                 setattr(self, k, v)
 
+    def postcmd(self):
+        sh.sleep(5)
+
+
 class MongoMongos(MongoCmd):
+
     def __init__(self, d, mgr):
         super(MongoMongos, self).__init__(d, mgr)
         self.priority = 2
 
     def cmd(self):
-        pass
+        sh.nohup(
+            'mongos',
+            port=self.port, configdb='localhost:' + str(self.mgr.config.port), _bg=True)
+
 
 class MongoConfig(MongoCmd):
+
     def __init__(self, d, mgr):
         super(MongoConfig, self).__init__(d, mgr)
         self.priority = 3
 
     def cmd(self):
-        pass
+        sh.nohup(
+            'mongod',
+            dbpath=self.path, port=self.port, smallfiles=True, _bg=True)
+
 
 class MongoMongod(MongoCmd):
+
     def __init__(self, d, mgr):
         super(MongoMongod, self).__init__(d, mgr)
         self.priority = 1
 
     def cmd(self):
-        pass
-
+        sh.nohup(
+            'mongod',
+            dbpath=self.path, port=self.port, smallfiles=True, _bg=True)
 
 
 if __name__ == '__main__':
     mongo_mgr = MongoSeverManager('config.yaml')
-    pass
-
+    for c in mongo_mgr.cmd:
+        c.precmd()
+        c.cmd()
+        c.postcmd()
+    
+    while True:
+        pass
